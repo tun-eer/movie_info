dd8d51755313b29a7386a96397ec676dfbbbf88f
Tuneer Upadhyay <slimshady@TUNEERs-Macbook-Air.local>
2020-05-12 15:48:56 +0530

    final commit

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About(props) {
+  return (
+    <div className='about__container'>
+      <span>“ To do a great right do a little wrong. "</span>
+      <span>− Tuneer Upadhyay, 2020</span>
+    </div>
+  );
+}
+
+export default About;
